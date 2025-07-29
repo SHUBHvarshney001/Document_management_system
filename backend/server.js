b2b43895389c946eb5b94d673b879feca1a469d8
SHUBHvarshney001 <122209368+SHUBHvarshney001@users.noreply.github.com>
2025-07-30 01:45:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const cors = require("cors")
 require("dotenv").config()
 
 const app = express()
-const PORT = process.env.PORT || 5500
+const PORT = process.env.PORT || 5000
 
 // Middleware
 app.use(
